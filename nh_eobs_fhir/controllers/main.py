aa7b8a40375fb3a9fd7d61165558921e48c048f6
Colin Wren <colin@tactix4.net>
2014-11-24 15:19:38 +0000

    Formatting FHIR API to be like Blackpear's output

--- nh_eobs_fhir/controllers/main.py
+++ nh_eobs_fhir/controllers/main.py
@@ -10,7 +10,7 @@ from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
 class FHIRAPI(http.Controller):
 
 
-    @http.route('/api/v1/patient', type='http', auth='none')
+    @http.route('/api/v1/Patient', type='http', auth='none')
     def search_patients(self, *args, **kw):
         uid = self.authenticate(request)
         if uid:
@@ -23,8 +23,17 @@ class FHIRAPI(http.Controller):
             patient_ids = request.session.model('nh.clinical.patient').search(search_domain)
             patients = []
             for patient_id in patient_ids:
-                patients.append(self.process_patient_fhir(patient_id))
-                return request.make_response(json.dumps(patients), headers={'Content-Type': 'application/json'})
+                patient_to_add = {}
+                patient_to_add['id'] = patient_id
+                patient_to_add['category'] = []
+                patient_to_add['content'] = self.process_patient_fhir(patient_id)
+                patients.append(patient_to_add)
+            search_response = {}
+            search_response['resourceType'] = 'Bundle'
+            search_response['title'] = 'Search result'
+            search_response['link'] = [{'rel': 'self', 'href': request.httprequest.url}]
+            search_response['entry'] = patients
+            return request.make_response(json.dumps(search_response), headers={'Content-Type': 'application/json'})
         else:
             return Response('Could not verify your access level for that URL.\n'
                             'You have to login with proper credentials', 401,
@@ -34,7 +43,9 @@ class FHIRAPI(http.Controller):
     def read_patients(self, patient_id, *args, **kw):
         uid = self.authenticate(request)
         if uid:
-            return request.make_response(json.dumps(self.process_patient_fhir(int(patient_id))), headers={'Content-Type': 'application/json'})
+            patient = self.process_patient_fhir(int(patient_id))
+            patient['resourceType'] = 'Patient'
+            return request.make_response(json.dumps(patient), headers={'Content-Type': 'application/json'})
         else:
             return Response('Could not verify your access level for that URL.\n'
                             'You have to login with proper credentials', 401,
@@ -45,11 +56,11 @@ class FHIRAPI(http.Controller):
         #patient_info = request.session.model('nh.eobs.api').get_patients([int(patient_id)])
         if len(raw_patient_info) > 0:
             patient_fhir = self.fhir_skeleton()
-            patient_fhir['resourceType'] = 'Patient'
+            # patient_fhir['resourceType'] = 'Patient'
             patient = raw_patient_info[0]
             patient_fhir['name'] = self.process_patient_name(patient)
             patient_fhir['identifier'] = self.process_patient_identifier(patient)
-            patient_fhir['telecom'] = self.process_patient_telecom(patient)
+            # patient_fhir['telecom'] = self.process_patient_telecom(patient)
             patient_fhir['gender'] = self.process_patient_gender(patient)
             patient_fhir['birthDate'] = self.process_patient_birthdate(patient)
             patient_fhir['address'] = self.process_patient_address(patient)
@@ -58,17 +69,17 @@ class FHIRAPI(http.Controller):
     def fhir_skeleton(self):
         fhir = {}
         fhir['resourceType'] = ''
-        fhir['text'] = {}
+        # fhir['text'] = {}
         fhir['identifier'] = []
         fhir['name'] = []
-        fhir['telecom'] = []
+        # fhir['telecom'] = []
         fhir['gender'] = {}
         fhir['birthDate'] = ''
-        fhir['deceasedBoolean'] = False
+        # fhir['deceasedBoolean'] = False
         fhir['address'] = []
         #fhir['contact'] = []
         #fhir['managingOrganization'] = {}
-        fhir['active'] = True
+        # fhir['active'] = True
         return fhir
 
     def process_patient_name(self, patient):
@@ -78,9 +89,11 @@ class FHIRAPI(http.Controller):
             uname = {}
             oname['use'] = 'official'
             uname['use'] = 'usual'
+            oname['prefix'] = patient['title'][1] if patient['title'] else ''
             uname['given'] = patient['given_name'].split(',')
             oname['family'] = patient['family_name'].split('-')
             oname['given'] = patient['given_name'].split(',')
+            oname['text'] = patient['full_name']
             if patient['middle_names']:
                 middle_names = patient['middle_names'].split(',')
                 for mname in middle_names:
@@ -127,13 +140,14 @@ class FHIRAPI(http.Controller):
     def process_patient_birthdate(self, patient):
         dob = patient['dob']
         date_of_birth = datetime.strptime(dob, DTF)
-        return datetime.strftime(date_of_birth, '%Y-%m-%d')
+        return datetime.strftime(date_of_birth, '%Y-%m-%dT%H:%M:%S.000Z')
 
     def process_patient_address(self, patient):
         addresses = []
         if patient['street'] or patient['street2'] or patient['city'] or patient['state_id'] or patient['zip']:
             home_address = {}
             home_address['use'] = 'home'
+            text_address = []
             if patient['street'] or patient['street2']:
                 line = []
                 if patient['street']:
@@ -141,12 +155,18 @@ class FHIRAPI(http.Controller):
                 if patient['street2']:
                     line.append(patient['street2'])
                 home_address['line'] = line
+                text_address.append(', '.join(line))
             if patient['city']:
                 home_address['city'] = patient['city']
+                text_address.append(patient['city'])
             if patient['state_id']:
                 home_address['state'] = request.session.model('res.country.state').read(patient['state_id'])['display_name']
+                text_address.append(patient['state'])
             if patient['zip']:
                 home_address['zip'] = patient['zip']
+                text_address.append(patient['zip'])
+
+            home_address['text'] = ', '.join(text_address)
             addresses.append(home_address)
         return addresses
 
